e4d6750f20ab0ab6ddbe3b84bec9bad5b96115f9
adrns012 <adrns012@gmail.com>
2022-04-04 15:12:10 -0400

    1st commit, Photo model WIP

---
+++ src/main/java/com/revature/Project2_Hawky/models/Photo.java
@@ -0,0 +1,26 @@
+package com.revature.Project2_Hawky.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "photos")
+public class Photo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "url", nullable = false, unique = true)
+    private String url;
+    private Integer postId;
+
+    @Column(name = "photo_url", nullable = false, length = 333)
+    private String photoUrl;
+}
