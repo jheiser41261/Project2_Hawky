ce5fe0f651d40c7cc2731529b9a2f60ec35e4008
Andy Nguyen <vutung1991@gmail.com>
2022-04-06 14:46:02 -0400

    andy added post to repos

---
+++ src/main/java/com/revature/Project2_Hawky/repos/PostDAO.java
@@ -0,0 +1,15 @@
+package com.revature.Project2_Hawky.repos;
+
+import com.revature.Project2_Hawky.models.Post;
+import com.revature.Project2_Hawky.models.User;
+
+public interface PostDAO {
+    Post getPostById(Integer postId);
+    Post getPostByUsername(User user);
+
+    Integer createPost(Post post);
+
+    void deletePost(Post post);
+
+    void updateLike(Post post);
+}
