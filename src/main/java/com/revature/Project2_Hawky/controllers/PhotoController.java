a655defac97ed8c1ca191e2b2baf725e749fcd26
adrns012 <adrns012@gmail.com>
2022-04-07 15:44:52 -0400

    Created photo controller

--- src/main/java/com/revature/Project2_Hawky/controllers/PhotoController.java
+++ src/main/java/com/revature/Project2_Hawky/controllers/PhotoController.java
@@ -1,4 +1,29 @@
 package com.revature.Project2_Hawky.controllers;
 
+import com.revature.Project2_Hawky.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/photo")
 public class PhotoController {
+
+    @Autowired
+    private UserController userController;
+    private PostService postService;
+
+    public PhotoController(PostService postService) {
+        this.postService = postService;
+    }
+
+
+
+
+
+
+
 }
+
+
--- src/main/java/com/revature/Project2_Hawky/repos/PhotoDAO.java
+++ src/main/java/com/revature/Project2_Hawky/repos/PhotoDAO.java
@@ -2,10 +2,8 @@ package com.revature.Project2_Hawky.repos;
 
 import com.revature.Project2_Hawky.models.Photo;
 import org.springframework.stereotype.Repository;
-
 import java.io.File;
 
-@Repository
 public interface PhotoDAO {
 
     void save(File photo);
