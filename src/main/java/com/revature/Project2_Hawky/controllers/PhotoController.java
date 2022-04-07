cb174b32e499f8cac8e99977a151856e20a51fb0
Paulina W <pmweintraub@gmail.com>
2022-04-07 16:07:51 -0400

    changes to photo controller

--- src/main/java/com/revature/Project2_Hawky/controllers/PhotoController.java
+++ src/main/java/com/revature/Project2_Hawky/controllers/PhotoController.java
@@ -1,28 +1,44 @@
 package com.revature.Project2_Hawky.controllers;
 
-import com.revature.Project2_Hawky.services.PostService;
+import com.revature.Project2_Hawky.models.Photo;
+import com.revature.Project2_Hawky.services.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
 
 @RestController
-@RequestMapping("/photo")
+@RequestMapping("photo")
 public class PhotoController {
 
     @Autowired
-    private UserController userController;
-    private PostService postService;
+    private PhotoService photoService;
 
-    public PhotoController(PostService postService) {
-        this.postService = postService;
+    public PhotoController(PhotoService photoService) {
+        this.photoService = photoService;
     }
 
+    @PostMapping
+    public Photo save(@RequestBody File photo){
+        return photoService.save(photo);
+    }
 
+    @GetMapping("{photoId}")
+    public Photo findById(@PathVariable Integer photoId){
+        return photoService.findById(photoId);
+    }
 
+    @PatchMapping("")
+    public Photo update(@PathVariable File photo){
+        return photoService.update(photo);
+    }
 
+    @DeleteMapping("{photo}")
+    public String delete(@PathVariable File photo){
+        this.photoService.delete(photo);
 
-
+        return "photo deleted";
+    }
 
 }
 
