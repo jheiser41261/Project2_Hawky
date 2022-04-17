6cbb95e96254a62ec56b24bd7343dfc7b9a1f5b6
Justin Heiser <justin.a.heiser@gmail.com>
2022-04-17 17:32:47 -0400

    added components and routes

--- src/main/resources/hawky-application/src/app/app-routing.module.ts
+++ src/main/resources/hawky-application/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { ProfilePageComponent } from './components/profiles/profile-page/profile-page.component';
+import { LoginComponent } from './components/sessions/login/login.component';
+import { RegisterComponent } from './components/sessions/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "dashboard", component: DashboardComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "profile", component: ProfilePageComponent},
+
+  {path: "", component: LoginComponent},
+  
+  {path: "**", redirectTo: ""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/resources/hawky-application/src/app/app.module.ts
+++ src/main/resources/hawky-application/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostContainerComponent } from './components/posts/post-container/post-container.component';
+import { ProfilePageComponent } from './components/profiles/profile-page/profile-page.component';
+import { PostItemComponent } from './components/posts/post-item/post-item.component';
+import { LoginComponent } from './components/sessions/login/login.component';
+import { RegisterComponent } from './components/sessions/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostContainerComponent,
+    ProfilePageComponent,
+    PostItemComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
